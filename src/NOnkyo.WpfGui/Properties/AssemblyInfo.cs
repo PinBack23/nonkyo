b9b63516fe0fb869d5d73425b705d48fc961d63e
KaSpar <karl.sparwald@gmail.com>
2013-01-24 13:22:40 +0100

    Update version to 0.9

--- src/NOnkyo.WpfGui/Properties/AssemblyInfo.cs
+++ src/NOnkyo.WpfGui/Properties/AssemblyInfo.cs
@@ -51,5 +51,5 @@ using System.Windows;
 // Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.8.7.1311")]
-[assembly: AssemblyFileVersion("0.8.7.1311")]
+[assembly: AssemblyVersion("0.9.1.1320")]
+[assembly: AssemblyFileVersion("0.9.1.1320")]
