c1fb44860c84e8c7c51461cc1e7861feb2fc8337
KaSpar <ks@abraxas.de>
2015-05-29 16:28:54 +0200

    - New controller for raw commands - New icons used

---
+++ src/NOnkyo.WpfGui/Controller/CommandController.cs
@@ -0,0 +1,21 @@
+﻿using NOnkyo.ISCP;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace NOnkyo.WpfGui.Controller
+{
+    public class CommandController : ApiController
+    {
+        [HttpGet]
+        public IHttpActionResult Send([FromUri] string command)
+        {
+            var loConnection = App.Container.Resolve<IConnection>();
+            loConnection.SendPackage(command);
+            return Ok();
+        }
+    }
+}
